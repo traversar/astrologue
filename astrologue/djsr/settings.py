ab21d639caf09f18056f9cd08a7f9400d4c95ec8
Andrew <travers.drew@gmail.com>
2021-01-03 09:31:56 -0500

    Django debug fix

--- astrologue/djsr/settings.py
+++ astrologue/djsr/settings.py
@@ -23,9 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
@@ -164,3 +161,6 @@ AUTH_USER_MODEL = 'authentication.CustomUser'
 if os.environ['NODE_ENV'] == 'production':
     import django_heroku
     django_heroku.settings(locals())
+    DEBUG = False
+else:
+    DEBUG = True
